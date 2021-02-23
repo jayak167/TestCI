0ece6bf175977ba77eb0d20371c1a81f5da6c2e4
jayak167 <jayak167@gmail.com>
2021-02-23 19:05:53 +0530

    fresh commit

---
+++ src/main/java/Daily/TestCI/App.java
@@ -0,0 +1,13 @@
+package Daily.TestCI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Daily/TestCI/TestA.java
@@ -0,0 +1,10 @@
+package Daily.TestCI;
+
+public class TestA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("This is Continous Integration");
+	}
+
+}
